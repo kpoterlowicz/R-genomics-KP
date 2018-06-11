15853366eb7c06dc68e9b6ca9fae11e0d66bcbe4
daltis20012009 <k.poterlowicz@gmail.com>
2018-06-11 13:30:46 +0100

    update

--- setup.R
+++ setup.R
@@ -2,27 +2,10 @@
 if (!file.exists("data"))
   dir.create("data")
 
-if (!file.exists("data / Ecoli_metadata.csv")) {
+if (!file.exists("data/Ecoli_metadata.csv")) {
   download.file("https://raw.githubusercontent.com/datacarpentry/R-genomics/gh-pages/data/Ecoli_metadata.csv","data / Ecoli_metadata.csv")
 }
-if (!file.exists("data / species.csv")){
-download.file("https: /  / ndownloader.figshare.com / files / 3299483",
-"data / species.csv")
-}
-if (!file.exists("data / plots.csv")) {
-download.file("https: /  / ndownloader.figshare.com / files / 3299474",
-"data / plots.csv")
-}
-if (!file.exists("data / portal_data_joined.csv")) {
-download.file("https: /  / ndownloader.figshare.com / files / 2292169",
-"data / portal_data_joined.csv")
-}
-
-if (!file.exists("data / portal_mammals.sqlite")) {
-download.file("https: /  / ndownloader.figshare.com / files / 2292171",
-"data / portal_mammals.sqlite")
-
-}
+ 
 
 
 ## knitr options
